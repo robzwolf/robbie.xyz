ebb329f08acb82a267db92a065da9e6c48700540
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2019-08-19 11:35:42 +0100

    Remove Typeform

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,4 @@
 // Styles for building
 import '../sass/style.scss';
 
-// Other JS files for building
-import './typeform';
-
 console.log("If you're reading this, you're the kind of person we need at DurHack! Check out https://durhack.com!");
